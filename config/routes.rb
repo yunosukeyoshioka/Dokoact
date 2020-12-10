6bc52d2296c27694785dfdc861155a30b95d8db1
vagrant <vagrant@localhost.localdomain>
2020-12-10 04:02:07 +0000

    reset_seed

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
 
   devise_for :users
   root 'home#top'
-  
+
   devise_scope :user do
     post '/sessions/guest_sign_in', to: 'sessions#new_guest'
-  end  
+  end
 
   resources :users, only: [:show, :create, :edit, :update, :new, :destroy, :unscribe, :withdraw] do
     collection do
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
   end
 
   resources :properties, only: [:new, :index, :show, :search, :create, :destroy, :my_property] do
-    collection do 
+    collection do
       get :search
       get :my_property
     end
@@ -25,11 +25,10 @@ Rails.application.routes.draw do
   end
 
   resources :messages, only: [:create, :destroy]
-	resources :rooms, only: [:index, :show, :create] 
-    
-  
+	resources :rooms, only: [:index, :show, :create]
 
-  resources :favorites, only: [:index, :create, :destroy] 
+
+  resources :favorites, only: [:index, :create, :destroy]
 
   resources :prefectures, only: [:create]
   resources :areas, only: [:create]
@@ -38,7 +37,7 @@ Rails.application.routes.draw do
   resources :genre, only: [:create], as: 'genres'
 
  	get 'about' => 'home#about', as: 'abouts'
-  
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
